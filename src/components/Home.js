e7fd928486bb8017b17caf6ada9ac13c16fb694c
tmpuser <olexiychebotkevich@gmail.com>
2019-06-05 16:54:49 +0200

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import NavMenu from './NavMenu';
+class Home extends Component {
+    state = {  }
+    render() { 
+        return ( <div>
+            <h1>Hello Home</h1>
+        </div> );
+    }
+}
+ 
+export default Home;
---
+++ src/components/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavMenu from './NavMenu';
+
+
+
+export default props => (
+    <div>
+      <NavMenu />
+      <div className="container">
+        {props.children}
+      </div>
+    </div>
+  );
